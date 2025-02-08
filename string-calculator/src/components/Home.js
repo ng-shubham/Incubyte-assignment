259281241ae6559a21386b3525792bb9f9d05565
ng-shubham <ngshubham01@gmail.com>
2025-02-08 19:57:44 +0530

    UI part implemented | Shubham Ambhore

--- string-calculator/src/components/Home.js
+++ string-calculator/src/components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className='row'>
+            <div className='offset-md-4 col-md-4 px-5'>
+                <h3 className='my-3'>String Calculator</h3>
+                <input type='text' className='form-control mb-3' placeholder='Enter String' />
+                <button className='btn btn-primary w-100'>Submit String</button>  
+                <div class="alert alert-success mt-2" role="alert">
+                    {"String output"}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Home
